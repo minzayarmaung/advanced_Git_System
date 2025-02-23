b75fa15609de19d545f3f00b13bdedf5a6682082
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-02-23 22:24:50 +0630

    newFolder

---
+++ gitSystem/src/app/components/new-folder/new-folder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-folder',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-folder.component.html',
+  styleUrl: './new-folder.component.css'
+})
+export class NewFolderComponent {
+
+}
