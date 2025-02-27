31a9c0297da5102ac93998ac592cd539b622e7a9
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-02-27 20:18:32 +0630

    change color component

---
+++ gitSystem/src/app/components/change-color/change-color.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-color',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-color.component.html',
+  styleUrl: './change-color.component.css'
+})
+export class ChangeColorComponent {
+
+}
